9f866333e55dbafab28c8a72b5dfc632992c32b3
James LaFarr <lafarrjam99@gmail.com>
2022-12-19 22:54:30 -0500

    Added is_valid()

--- bst.hpp
+++ bst.hpp
@@ -2,6 +2,7 @@
 #define BST_HPP
 
 #include <stdexcept>
+#include <algorithm>
 #include "bstnode.hpp"
 
 template <typename T>
@@ -37,7 +38,7 @@ class bst {
          * @return true if value is contained in the tree, false otherwise
          */
         bool contains(T data) {
-            return this->root ? this->root->contains(data) : false;
+            return this->root && this->root->contains(data);
         }
         
         /**
@@ -58,20 +59,41 @@ class bst {
             return this->root ? this->root->left->num_left() + this->root->right->num_left() : 0;
         }
 
+        /**
+         * Determines if the tree is a balanced binary search tree.
+         *
+         * @return true if the tree is balanced, false otherwise
+         */
         bool is_balanced() {
             return is_balanced_helper(root);
         }
 
-        // TODO: IMPLEMENT is_valid()
+        /**
+         * Determines if the current tree is a valid binary search tree.
+         *
+         * @param n the current node
+         * @return true if the current tree is a valid binary search tree, false otherwise
+         */
+        bool is_valid(bstnode<T> *n) {
+            if (!n) {
+                return true;
+            } else if (!is_valid(n->left) || !is_valid(n->right)) {
+                return false;
+            } else {
+                bool left = !n->left || n->left->data < n->data;
+                bool right = !n->right || n->right->data >= n->data;
+                return left && right;
+            }
+        }
 
     private:
         /** The root of the tree */
         bstnode<T> *root;
 
         /** The number of nodes in the tree */
-        int size;
+        int size{};
 
-	bool balanced_helper(bstnode *n) {
+	bool balanced_helper(bstnode<T> *n) {
 		if (!n) {
 			return true;
 		} else if (!balanced_helper(n->left) || !balanced_helper(n->right)) {
@@ -81,7 +103,7 @@ class bst {
 		}
 	}
 
-    int level_helper(bstnode *node) {
+    int level_helper(bstnode<T> *node) {
         if (!node) {
             return 0;
         } else if (node->left && node->right) {
